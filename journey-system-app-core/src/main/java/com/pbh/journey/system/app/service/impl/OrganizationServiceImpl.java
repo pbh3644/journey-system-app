084e06e8295bd10ad8611b0608b08fb87df2cdec
ex-pangb001 <pbh.3644>
2018-10-23 14:43:29 +0800

    修改pom文件依赖

--- journey-system-app-core/src/main/java/com/pbh/journey/system/app/service/impl/OrganizationServiceImpl.java
+++ journey-system-app-core/src/main/java/com/pbh/journey/system/app/service/impl/OrganizationServiceImpl.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
